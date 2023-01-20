12e8449113e6389c4445db7cae3f20bb2e7f0104
Arnaud <arnaud91berrier@gmail.com>
2023-01-20 19:19:15 +0100

    PasswordHasher

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,13 @@
 
 namespace App\DataFixtures;
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {   
@@ -15,6 +16,8 @@ class AppFixtures extends Fixture
     * @var Generator
     */
     private Generator $faker;
+
+    
 
     public function __construct()
     {
@@ -51,6 +54,18 @@ class AppFixtures extends Fixture
                     }
 
                     $manager->persist($recipe);
+        }
+        // Users
+
+        for ($i=1; $i < 10 ; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE USER'])
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
         }
         $manager->flush();
     }
